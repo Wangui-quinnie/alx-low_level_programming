afa5682043d61ac70e9c6b24341301c653c883af
Wangui-quinnie <w.wangui@ymail.com>
2023-03-21 10:50:35 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  *main - entry point
+  *
+  *Description: prints _putchar
+  *
+  *Return: Always 0
+  *
+  */
+int main(void)
+{
+	_putchar('_');
+	_putchar('P');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
