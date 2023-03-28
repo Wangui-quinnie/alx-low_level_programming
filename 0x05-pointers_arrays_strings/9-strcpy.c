cac24fcd1061ae1e97fc300ee72e67030aa9ed4a
Wangui-quinnie <w.wangui@ymail.com>
2023-03-28 13:10:50 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
   *_strcpy - copies the string pointed to by src, including the null byte
@@ -14,6 +15,8 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
+	if (dest == NULL)
+		return (NULL);
 	for (i = 0; src[i] != '\0'; i++)
 		dest[i] = src[i];
 	return (dest);
