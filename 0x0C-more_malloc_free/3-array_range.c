86bfb245867057365185a809f220a7c95c1bbd7d
Wangui-quinnie <w.wangui@ymail.com>
2023-04-14 06:45:24 -0700

    betty fix

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,7 +18,7 @@ int *array_range(int min, int max)
 	array = malloc(sizeof(int) * size);
 	if (array == NULL)
 		return (NULL);
-	for (i = 0;i < size; i++)
+	for (i = 0; i < size; i++)
 		array[i] = min + i;
 	return (array);
 }
