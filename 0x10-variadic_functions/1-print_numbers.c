84e3c703349883de3fb969d14218488393d209a9
Wangui-quinnie <w.wangui@ymail.com>
2023-04-20 17:30:50 -0700

    To be is to be the value of a variable

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 #include <stdio.h>
 
 
@@ -6,6 +7,7 @@
   *print_numbers - prints numbers followed by a new line
   *@separator: the string to be printed between numbers
   *@n: the number of integers passed to the function
+  *@...: variable arguments list list of integers to be printed
   *Return: void
   */
 
@@ -18,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d ", va_arg(args, int));
+		printf("%d", va_arg(args, int));
 		if (i != (n - 1) && separator != NULL)
 		printf("%s", separator);
 	}
