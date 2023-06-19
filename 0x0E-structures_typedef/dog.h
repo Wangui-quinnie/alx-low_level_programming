1dcb289bd4e5fa9783e2f3e179be07fd3ab8efc7
Wangui-quinnie <w.wangui@ymail.com>
2023-06-19 11:25:54 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  *struct dog - a dog's basic info
+  *@name: name of the dog
+  *@age: the dog's age
+  *@owner: the dog's owner
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
