7d3952c871073f6fbbb3523c20808fbbecebc93d
Wangui-quinnie <w.wangui@ymail.com>
2023-09-08 17:18:06 -0700

    >>> ht['betty']

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+  *hash_table_get - retrieves a value associated with a key
+  *@ht: the hash table
+  *@key: the key to search for
+  *Return: value associated with the element or NULL if key
+  *couldn't be found
+  */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
+}
