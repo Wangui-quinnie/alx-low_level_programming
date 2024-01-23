19ab849896858409df26af797680da9c522c1486
Wangui-quinnie <w.wangui@ymail.com>
2024-01-22 22:10:44 -0800

    Jump search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -42,6 +42,7 @@ typedef struct skiplist_s
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+size_t min(size_t a, size_t b);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
